2c20e93fc9a48f15dc297fb48b9f829e4de0da70
joshduo <josh142011@gmail.com>
2021-08-01 08:55:22 -0700

    Change resume embed source

--- js/resume.js
+++ js/resume.js
@@ -6,12 +6,12 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
     // fallbackLink is inserted into the page when the browser doesn't have built-in PDF-viewing
     let options = {fallbackLink: '<iframe id="resume-iframe"\
-     src="https://docs.google.com/viewer?url=https://github.com/joshduo/resume/raw/main/Joshua_Duong_Resume.pdf&embedded=true"\
-     title="Web view of Joshua Duong\'s resume through Google Docs Viewer"></iframe>'};
+    src="https://docs.google.com/viewer?url=https://github.com/joshduo/resume/raw/main/Joshua_Duong_Resume.pdf&embedded=true"\
+    title="Web view of Joshua Duong\'s resume through Google Docs Viewer"></iframe>'};
     let iframeTimer;
     
     // Attempt to embed my resume into the page, using fallbackLink if not supported
-    PDFObject.embed("Joshua_Duong_Resume.pdf", "#resume-pdf", options);
+    PDFObject.embed('https://nbviewer.jupyter.org/github/joshduo/resume/blob/main/Joshua_Duong_Resume.pdf', '#resume-pdf', options);
 
     // If the browser doesn't have built-in PDF viewing, set reload timer for fallback
     if(!PDFObject.supportsPDFs) {
@@ -22,7 +22,7 @@ function init() {
         document.getElementById('resume-iframe').addEventListener('load', function() {
             document.getElementById('loading').style.display = 'none';
             clearInterval(iframeTimer);
-            console.log("iframe finally loaded");
+            console.log('iframe finally loaded');
         })
     }
 }
@@ -31,18 +31,12 @@ function init() {
  * Reload the iframe if it doesn't load correctly.
  */
 function reloadIFrame() {
-    console.log("iframe not loaded. reloading iframe...");
+    console.log('iframe not loaded. reloading iframe...');
     let iframe = document.getElementById('resume-iframe');
 
-    /*
-    if (iframe.contentDocument === null) {
-        console.log("url not loaded yet; waiting");
-    }
-    */
-
     // If the document has loaded and the iframe page is empty, reload it. 
-    if (iframe.contentDocument !== null && iframe.contentDocument.URL === "about:blank") {
-        console.log("reloading");
+    if (iframe.contentDocument !== null && iframe.contentDocument.URL === 'about:blank') {
+        console.log('reloading');
         iframe.src = iframe.src;
     }
 }
